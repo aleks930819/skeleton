75f72f897027d56d053ec52c45b5721ab0b03c7d
Aleksandar Atanasov <aleks930819@gmail.com>
2023-04-23 19:02:11 +0300

    fix

--- src/utils/logoutHandler.js
+++ src/utils/logoutHandler.js
@@ -1,9 +1,7 @@
 import { logout } from '../data/auth.js';
 import { page } from '../utils/lib.js';
-import { navController } from './navController.js';
 
 export const logoutHandler = () => {
   logout();
-  navController();
   page.redirect('/');
 };
